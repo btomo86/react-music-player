b67c4208a1c05001643a775a0ba1fe0a616f29b9
Ben Thompson <btomo86@gmail.com>
2020-12-21 12:41:57 +0000

    fixed the bugs

--- src/components/Player.js
+++ src/components/Player.js
@@ -36,8 +36,8 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   };
 
   const [songInfo, setSongInfo] = useState({
-    currentTime: null,
-    duration: null,
+    currentTime: 0,
+    duration: 0,
   });
   return (
     <div className="player">
